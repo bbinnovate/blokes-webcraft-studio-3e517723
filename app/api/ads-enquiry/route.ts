ba790e2ad3eda60e9c664f00bb0b90b2e820ab32
Aryan Kuril <aryan@bombayblokes.com>
2026-09-16 18:24:29 +0530

    admin email added

--- app/api/ads-enquiry/route.ts
+++ app/api/ads-enquiry/route.ts
@@ -293,11 +293,11 @@ export async function POST(req: Request) {
     });
 
     await sendEmail({
-      to: "aryan@bombayblokes.com",
-      // to: ["hello@bombayblokes.com", 
-      //   "bdm@bombayblokes.com", 
-      //   "siddique@bombayblokes.com", 
-      //   "aryankuril09@gmail.com"],
+      // to: "aryan@bombayblokes.com",
+      to: ["hello@bombayblokes.com", 
+        "bdm@bombayblokes.com", 
+        "siddique@bombayblokes.com", 
+        "aryankuril09@gmail.com"],
       subject: `New Lead From - ${formatTitleCase(payload.name || "-")} for Website Development`,
       html: buildAdminEmail(payload),
       fromName: "BB Forms",
